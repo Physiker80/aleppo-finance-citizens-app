5e374e3e0ebfe50db70db9b51d653bf146e3dbe7
Physiker80 <sedki.noureddin@gmail.com>
2026-02-03 17:30:33 +0100

    Regenerate Android project with fixed gradle, add notification system integration, version 1.1

--- android/download-wrapper.bat
+++
@@ -1,8 +0,0 @@
-
-@echo off
-cd /d %~dp0
-echo Downloading gradle-wrapper.jar...
-curl -L -o "gradle\wrapper\gradle-wrapper.jar" "https://raw.githubusercontent.com/nicheware/gradle-wrapper-jar/gradle-8.0/gradle/wrapper/gradle-wrapper.jar"
-echo Done! Checking file size:
-dir "gradle\wrapper\gradle-wrapper.jar"
-pause
